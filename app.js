217923876bb0d81231611ee7b9a077318aa3d988
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-05-14 11:46:17 -0400

    going to embed playlist instead opf use API

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const APIController = (function() {
     
-    const clientId = 'ADD YOUR CLIENT ID';
-    const clientSecret = 'ADD YOUR CLIENT SECRET';
+    const clientId = 'd5cb1a5cfd304e1daad3362fb3b231e5';
+    const clientSecret = '92090381e11845c2ab551eb6fac53368';
 
     // private methods
     const _getToken = async () => {
